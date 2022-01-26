958e5f7be2fe52bff262ab978ced4f62ca8f7bc7
Daimel Moreno <Daimel@Jinas-MacBook-Pro.local>
2022-01-26 14:28:14 -0500

    make it better

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const cors = require('cors')
 const authRoutes = require('./routes/auth.js')
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 
 //requiring environment variables
 require('dotenv').config()
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
-const twilioClient = require('twilio')(accountSid, authToken);
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+// const twilioClient = require('twilio')(accountSid, authToken);
 
 //middleware
 app.use(cors())
@@ -27,26 +27,26 @@ app.get('/', (req, res) => {
 
 
 //twilio code
-app.post('/', (req, res) => {
-    const { message, user: sender, type, members } = req.body;
-    if (type === 'message.new') {
-        members
-            .filter((member) => member.user_id !== sender.id )
-            .forEach(({ user }) => {
-            if (!user.online) {
-                twilioClient.messages.create({
-                    body: `You have new message from ${message.user.fullName} - ${message.text}`,
-                    messagingServiceSid: messagingServiceSid,
-                    to: user.phoneNumber
-                })
-                    .then(() => console.log('Message sent!'))
-                    .catch((error) => console.log(error) )
-            }
-            })
-        return res.status(200).send('Message sent!');
-    }
-    return res.status(200).send('Not a new message request!');
-})
+// app.post('/', (req, res) => {
+//     const { message, user: sender, type, members } = req.body;
+//     if (type === 'message.new') {
+//         members
+//             .filter((member) => member.user_id !== sender.id )
+//             .forEach(({ user }) => {
+//             if (!user.online) {
+//                 twilioClient.messages.create({
+//                     body: `You have new message from ${message.user.fullName} - ${message.text}`,
+//                     messagingServiceSid: messagingServiceSid,
+//                     to: user.phoneNumber
+//                 })
+//                     .then(() => console.log('Message sent!'))
+//                     .catch((error) => console.log(error) )
+//             }
+//             })
+//         return res.status(200).send('Message sent!');
+//     }
+//     return res.status(200).send('Not a new message request!');
+// })
 
 app.use('/auth', authRoutes)
 
